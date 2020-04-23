8841567ebbb27249c64ad8b7d4dbc7470b11685a
Ravel Andrade <ravel.gou@gmail.com>
2020-04-23 05:54:21 -0300

    Projeto de fixação do pensionato finalizado

---
+++ Pensionato/Pensionato/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Pensionato {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Quantos quarto serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Quarto[] lista = new Quarto[10];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Digite o nome do cliente de numero "+i);
+                String nome = Console.ReadLine();
+                Console.WriteLine("Digite o email do cliente de numero " + i);
+                String email = Console.ReadLine();
+                int numero;
+
+                Console.WriteLine("Digite o quarto do cliente de numero " + i);
+
+                do {
+                     numero = int.Parse(Console.ReadLine());
+                    if (numero < 0 || numero > 10) {
+                        Console.WriteLine("Valor invalido, favor entre com um valor novamente (0-9)");
+                    }
+                    else {
+                        if (lista[numero] != null) {
+                            Console.WriteLine("Quarto indisponivel, insira outro quarto: ");
+                        }
+                    }
+                } while ((numero<0 || numero>10) || lista[numero]!=null);
+                
+                lista[numero] = new Quarto { Nome = nome, Email = email, Numero = numero };
+
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+
+            for (int i = 0; i < 10; i++) {
+                if (lista[i] != null) {
+                    Console.WriteLine(lista[i].Numero + ": " + lista[i].Nome + ", " + lista[i].Email);
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Pensionato/Pensionato/Quarto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pensionato {
+    class Quarto {
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Numero { get; set; }
+    }
+}
