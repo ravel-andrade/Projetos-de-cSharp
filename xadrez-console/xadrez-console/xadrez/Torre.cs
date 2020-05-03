f64178ac3d721f317426f2a32ed40c7b296d8105
Ravel Andrade <ravel.gou@gmail.com>
2020-05-03 19:31:55 -0300

    comecando a colocar pecas no tabuleiro

---
+++ xadrez-console/xadrez-console/xadrez/Torre.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(cor, tab)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return "T";
+        }
+
+    }
+}
