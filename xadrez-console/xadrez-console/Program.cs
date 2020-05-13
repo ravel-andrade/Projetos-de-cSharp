c83bdc9a6c339abedd9455187f28d2f76afc8f5d
Ravel Andrade <ravel.gou@gmail.com>
2020-05-13 11:48:40 -0300

    Novos métodos e exceptions de tabuleiro

--- xadrez-console/xadrez-console/Program.cs
+++ xadrez-console/xadrez-console/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using tabuleiro;
 using xadrez;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console
 {
@@ -9,14 +10,21 @@ namespace xadrez_console
         static void Main(string[] args)
         {
 
+            try {
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(0, 9));
 
-            Tabuleiro tab = new Tabuleiro(8, 8);
-
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preto), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preto), new Posicao(2, 4));
+                Tela.ImprimirTabuleiro(tab);
             
-            Tela.ImprimirTabuleiro(tab);
+            }catch(TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+            
+            
         }
     }
 }
