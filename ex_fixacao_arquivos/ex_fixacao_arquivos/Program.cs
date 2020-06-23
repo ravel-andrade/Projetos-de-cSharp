0ba9bac029d7f730ea12e7d81ffa30b24f076532
ravel-andrade <ravel.gou@gmail.com>
2020-06-23 20:02:16 -0300

    Inclusao do exercicio de fixacao para arquivos

---
+++ ex_fixacao_arquivos/ex_fixacao_arquivos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ex_fixacao_arquivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Source path: ");
+            string sourcePath = @Console.ReadLine();
+            try
+            {
+                string sourceFolderPath = Path.GetDirectoryName(sourcePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+                Directory.CreateDirectory(targetFolderPath);
+
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                foreach(String x in lines)
+                {
+                    string[] conteudos = x.Split(",");
+                    String final = conteudos[0] + ", " + (Double.Parse(conteudos[1]) * Double.Parse(conteudos[2]));
+                    using (StreamWriter sw = File.AppendText(targetFolderPath + @"\sumamary.csv"))
+                    {
+                        sw.WriteLine(final);
+                    }
+                }
+
+            }catch(IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
